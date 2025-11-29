7322d575569670d831f911fca4a7ca33f38e8065
Melvin Jones Repol <mrepol742@gmail.com>
2025-11-29 16:46:37 +0800

    feat: Iterating Through Option

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,45 @@
-// fn vowels(words: &str) -> u8 {
-//     let vowels_count: usize = words
-//         .chars()
-//         .into_iter()
-//         .filter(|x: &char| (*x == 'a') | (*x == 'e') | (*x == 'i') | (*x == 'o') | (*x == 'u'))
-//         .count();
-
-//     vowels_count as u8
-// }
-
-// fn main() {
-//     let affan = "affan".to_string();
-//     println!("{}: {:?}", affan, vowels(&affan));
-
-//      println!("Ferris: {}", vowels("Ferris"));
-// }
-
-// fn length_str(x: &str) {
-//     println!("length of the string is {} is {}", x, x.len());
-// }
-
-// fn main() {
-//     let box_str = Box::new("Hello");
-//     length_str(&box_str);
-
-//     length_str("Hello Rust");
-// }
-//
-
-fn square_values(num_vec: &[i32]) {
-    for i in num_vec {
-        println!("The square is {}", i);
-    }
-}
-
 fn main() {
-    let values_vec = vec![1,2,3,4,5,6];
-    let values_array = [1,2,3,4,5,6];
+    let some_product = Some("laptop");
+    let mut product_vec = vec!["cellphone", "battery", "charger"];
 
-    square_values(&values_vec);
-    square_values(&values_array);
+    // match some_product {
+    //     Some(product) => product_vec.push(product),
+    //     _ => ()
+    // }
+
+    // if let Some(product) = some_product {
+    //     product_vec.push(product);
+    // }
+
+    // product_vec.extend(some_product);
+    // println!("{:?}", product_vec);
+
+    // let products_iter = product_vec.iter().chain(some_product.iter());
+    // for prod in products_iter {
+    //     println!("{:?}", prod);
+    // }
+    //
+
+    let products = vec![Some("Carger"), Some("Battery"), Some("Cellphone")];
+    // let mut prod_without_none = Vec::new();
+
+    // for p in products {
+    //     if p.is_some() {
+    //         prod_without_none.push(p.unwrap());
+    //     }
+    // }
+    // println!("{:?}", prod_without_none);
+    //
+
+    // let prod_without_none = Box::new(products.into_iter())
+    //     .filter(|x|x.as_ref().is_some())
+    //     .map(|x|x.unwrap())
+    //     .collect::<Vec<&str>>();
+
+    // println!("{:?}", prod_without_none);
+    //
+
+    let products: Vec<&str> = products.into_iter().flatten().collect();
+    println!("{:?}", products);
+
 }
