0fd086cbd5b27697bec9c119a95b6d12859aa93a
Melvin Jones Repol <mrepol742@gmail.com>
2025-11-29 17:19:09 +0800

    feat: Disabling Mutability for Finalized Objects

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,54 @@
+// fn main() {
+//     // let mut data = vec![5,6,9,4,3];
+//     // data.sort();
+//     // let data = data;
+
+//     let data = {
+//         let mut data = vec![5,6,9,4,3];
+//         data.sort();
+//         data
+//     };
+
+// }
+#[derive(Debug, Clone)]
+
+pub struct finalized_config<T>(T);
+
+impl<T> Copy for finalized_config<T> where T: Copy {}
+
+impl<T> std::ops::Deref for finalized_config<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+struct Config {
+    a: usize,
+    b: String,
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            a: 0,
+            b: String::from("Hello"),
+        }
+    }
+
+    fn build(self) -> finalized_config<Config> {
+        finalized_config(self)
+    }
+}
+
 fn main() {
-    let some_product = Some("laptop");
-    let mut product_vec = vec!["cellphone", "battery", "charger"];
 
-    // match some_product {
-    //     Some(product) => product_vec.push(product),
-    //     _ => ()
-    // }
+    let mut my_configuration = Config::new();
+    my_configuration.a = 6;
 
-    // if let Some(product) = some_product {
-    //     product_vec.push(product);
-    // }
+    let finalized = my_configuration.build();
 
-    // product_vec.extend(some_product);
-    // println!("{:?}", product_vec);
-
-    // let products_iter = product_vec.iter().chain(some_product.iter());
-    // for prod in products_iter {
-    //     println!("{:?}", prod);
-    // }
-    //
-
-    let products = vec![Some("Carger"), Some("Battery"), Some("Cellphone")];
-    // let mut prod_without_none = Vec::new();
-
-    // for p in products {
-    //     if p.is_some() {
-    //         prod_without_none.push(p.unwrap());
-    //     }
-    // }
-    // println!("{:?}", prod_without_none);
-    //
-
-    // let prod_without_none = Box::new(products.into_iter())
-    //     .filter(|x|x.as_ref().is_some())
-    //     .map(|x|x.unwrap())
-    //     .collect::<Vec<&str>>();
-
-    // println!("{:?}", prod_without_none);
-    //
-
-    let products: Vec<&str> = products.into_iter().flatten().collect();
-    println!("{:?}", products);
-
+    let mut finalized_copy = finalized;
+    // finalized_copy.a = 66;
 }
