b32efcd03e26b46741137da040f3305d80abebe5
kigemmanuel <kigemmanuel.mail@gmail.com>
2025-11-30 07:36:35 +0100

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,53 @@
-// fn main() {
-//     // let mut data = vec![5,6,9,4,3];
-//     // data.sort();
-//     // let data = data;
+#![allow(dead_code)]
+#![allow(unused)]
 
-//     let data = {
-//         let mut data = vec![5,6,9,4,3];
-//         data.sort();
-//         data
-//     };
-
-// }
-#[derive(Debug, Clone)]
-
-pub struct finalized_config<T>(T);
-
-impl<T> Copy for finalized_config<T> where T: Copy {}
-
-impl<T> std::ops::Deref for finalized_config<T> {
-    type Target = T;
-    fn deref(&self) -> &T {
-        &self.0
-    }
+enum Device {
+    Iphone(i8),
+    Samsung(String),
 }
 
-#[derive(Debug)]
-struct Config {
-    a: usize,
-    b: String,
+enum Type {
+    Peck(i32),
+    VeryPeck(i32),
 }
 
-impl Config {
-    fn new() -> Self {
-        Self {
-            a: 0,
-            b: String::from("Hello"),
-        }
-    }
-
-    fn build(self) -> finalized_config<Config> {
-        finalized_config(self)
-    }
+enum Level {
+    Easy,
+    Hard,
+    Extreme,
+    Custom (Type),
 }
 
 fn main() {
+    
+    use crate::Device;
+    use crate::Type::{Peck, VeryPeck};
+    use crate::Level::Custom;
+    use crate::Level::Easy;
+    
+    let phone: Device = Device::Iphone(17);
+    let stage: Level = Easy;
+    let another_level: Level = Custom(VeryPeck(9_0));
+    
+    
+    check(stage);
+    check(another_level);
+    
+    match phone {
+        Device::Iphone(x) => println!("Phone:: {}", x),
+        Device::Samsung(x) => println!("Phone:: {}", x)
+    }
+}
 
-    let mut my_configuration = Config::new();
-    my_configuration.a = 6;
-
-    let finalized = my_configuration.build();
-
-    let mut finalized_copy = finalized;
-    // finalized_copy.a = 66;
+fn check (stage: Level) {
+    match stage {
+        Level::Easy => println!("Level:: Easy"),
+        Level::Custom(option) => {
+            match option {
+                Type::Peck(x) => println!("Custom:: Peck:: {}", x),
+                Type::VeryPeck(x) => println!("Custom:: VeryPeck:: {}", x)
+            }
+        },
+        _=> println!("Other:: Hard || Extreme")
+    } 
 }
