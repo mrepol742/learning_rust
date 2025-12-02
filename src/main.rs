167b3286d0264c9347eeb469cbdda8a79e3d1159
Melvin Jones Repol <mrepol742@gmail.com>
2025-12-02 15:48:06 +0800

    feat: Passing Mutexes between Threads

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,51 @@
+// use std::sync::Mutex;
+// use std::thread;
+// use std::sync::Arc;
+
+// fn main() {
+//     let counter = Arc::new(Mutex::new(0));
+//     let mut handles = vec![];
+
+//     for _ in 0..10 {
+//         let counter = Arc::clone(&counter);
+//         let handle = thread::spawn(move || {
+//             let mut num = counter.lock().unwrap();
+//             *num += 1;
+//         });
+
+//         handles.push(handle);
+//     }
+
+//     for handler in handles {
+//         handler.join().unwrap();
+//     }
+
+//     println!("Results: {}", *counter.lock().unwrap());
+// }
+
 use std::sync::Mutex;
+use std::thread;
+use std::sync::Arc;
+
+struct MyString(String);
+impl MyString {
+    fn new(s: &str) -> MyString {
+        MyString(s.to_string())
+    }
+}
 
 fn main() {
-    let m = Mutex::new(5);
+    let mut threads = Vec::new();
+    let name = Arc::new(MyString::new("melvin"));
+    for i in 0..5 {
+        let some_str = name.clone();
+        let t = thread::spawn(move || {
+            println!("hello {} count {}", some_str.0, i)
+        });
+        threads.push(t);
+    }
 
-    // {
-    //     let mut num = m.lock().unwrap();
-    //     *num = 10;
-
-    // }
-
-    // println!("m = {:?}", m);
-
-    let mut num = m.lock().unwrap();
-    *num = 10;
-    drop(num);
-
-    let mut num1 = m.lock().unwrap();
-    *num1 = 10;
-    drop(num1);
+    for t in threads {
+        t.join().unwrap();
+    }
 }
